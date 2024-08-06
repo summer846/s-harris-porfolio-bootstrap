fca21e5d63609b3d27395418245a42220fe35345
summer846 <summerh07032008@gmail.com>
2024-08-06 16:04:50 +0000

    built an age verification app on home page

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("Hello World")
+//console.log("Hello World")
+
+//var prompt = prompt("What is your name?")
+
+//console.log(prompt)
+
+
+
+function verifyAge (age) {
+  var age = prompt("What is your age?")
+  console.log(age)
+
+  if(age < 18) {
+    alert ('You are a minor!')
+  } else if (age>= 18 && age <= 60) {
+    alert ('You are an adult!')
+  } else {
+    alert ('You are a senior citizen!')
+  }
+}
